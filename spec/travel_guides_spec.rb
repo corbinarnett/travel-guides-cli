21e73b6b332cd4f3257987f0aace717d486dccf4
corbinarnett <corbin.c.arnett@gmail.com>
2019-09-05 13:50:18 -0700

    first commit

---
+++ spec/travel_guides_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TravelGuides do
+  it "has a version number" do
+    expect(TravelGuides::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
